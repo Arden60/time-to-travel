381b4d757d91e6575c31b1306a3c69b745f556ec
Arden60 <42158926+Arden60@users.noreply.github.com>
2019-02-06 11:47:01 +1000

    tweaks

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -48,6 +48,7 @@ function activeMenu() {
 
 
 
+
 }
 
 // now run the function
@@ -60,9 +61,8 @@ function imageGallery() {
     if (!$(".image-gallery").length) {
     return;
     /* this means that the page is looking for something with a class of 'image-gallery'
-    if it can't find one, it 'returns', which means it does nothing.
-    If it DOES find such a class (like on our media page), then it will
-    carry on and read the line below.
+    If it can't find one, it 'returns', which means it does nothing.
+    If it DOES find such a class (like on our media page), then it will carry on and read the line below.
     Without these lines above, this script would cause an error on pages without a gallery.
     */
     }
